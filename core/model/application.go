db7885154aa0047c7a01215988067fa73ad9972e
Ryan Oberlander <46940735+roberlander2@users.noreply.github.com>
2025-11-10 05:32:23 -0600

    [#811] Implement refresh token reuse detection grace period (#812)
    * add refresh grace period [#811]
    
    * only allow the previous refresh token to be used in the grace period [#811]
    
    * change grace period from minutes to seconds, return error on missing current or previous refresh token [#811]
    
    * better error handling [#811]
    
    * fix secrets
    
    * Set grace period policy in the api when get data
    
    ---------
    
    Co-authored-by: Petyo Stoyanov <petyo.stoyanov@inabit.eu>

--- core/model/application.go
+++ core/model/application.go
@@ -420,9 +420,10 @@ type LoginsSessionsSetting struct {
 	MaxConcurrentSessions       int                         `bson:"max_concurrent_sessions"`
 	AccessTokenExpirationPolicy AccessTokenExpirationPolicy `bson:"access_token_expiration_policy"`
 
-	InactivityExpirePolicy InactivityExpirePolicy `bson:"inactivity_expire_policy"`
-	TSLExpirePolicy        TSLExpirePolicy        `bson:"time_since_login_expire_policy"`
-	YearlyExpirePolicy     YearlyExpirePolicy     `bson:"yearly_expire_policy"`
+	InactivityExpirePolicy   InactivityExpirePolicy   `bson:"inactivity_expire_policy"`
+	TSLExpirePolicy          TSLExpirePolicy          `bson:"time_since_login_expire_policy"`
+	YearlyExpirePolicy       YearlyExpirePolicy       `bson:"yearly_expire_policy"`
+	RefreshGracePeriodPolicy RefreshGracePeriodPolicy `bson:"refresh_grace_period_policy"`
 }
 
 // AccessTokenExpirationPolicy represents the expiration policy for access tokens
@@ -451,6 +452,12 @@ type YearlyExpirePolicy struct {
 	Month  int  `bson:"month"`
 	Hour   int  `bson:"hour"`
 	Min    int  `bson:"min"`
+}
+
+// RefreshGracePeriodPolicy represents refresh token grace period policy
+type RefreshGracePeriodPolicy struct {
+	Active      bool `bson:"active"`
+	GracePeriod int  `bson:"grace_period"` //in seconds
 }
 
 // ApplicationType represents users application type entity - safer community android, safer community ios, safer community web, uuic android etc
